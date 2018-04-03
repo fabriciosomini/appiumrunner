9c756d585b87bcfb6c624426545b502fec34dfb1
fabri <fabricio.somini@gmail.com>
2018-04-03 17:20:31 -0300

    Correção: adicionado um elemento para remover o foco do campo de texto

--- app/src/test/java/appiumrunner/unesc/net/appiumrunner/ClasseTesteTCC3.java
+++ app/src/test/java/appiumrunner/unesc/net/appiumrunner/ClasseTesteTCC3.java
@@ -1,37 +1,34 @@
 package appiumrunner.unesc.net.appiumrunner;
 
-import junit.framework.Assert;
-
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 
 public class ClasseTesteTCC3 {
-    private WebDriver driver = null;
+    private AppiumDriver<MobileElement> driver = null;
     @Before
     public void setup() {
         File app = new File(".\\build\\outputs\\apk\\debug\\", "app-debug.apk");
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.1.2");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "4.4.4");
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "adroid");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        //capabilities.setCapability(MobileCapabilityType.APP_ACTIVITY, "appiumrunner.unesc.net.appiumrunner.activities.MainActivity");
         capabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
 
         try {
-            driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+            driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
@@ -41,37 +38,21 @@ public class ClasseTesteTCC3 {
     @Test
     public void teste() {
 
-        WebElement nomeMotorista = driver.findElement(By.id("nomeMotorista"));
+        MobileElement nomeMotorista = driver.findElement(By.id("nomeMotorista"));
         Assert.assertEquals(nomeMotorista.equals(driver.switchTo().activeElement()), true);
         Assert.assertEquals(nomeMotorista.getText(), "");
 
 
-        WebElement cpfMotorista = driver.findElement(By.id("cpfMotorista"));
+        MobileElement cpfMotorista = driver.findElement(By.id("cpfMotorista"));
         Assert.assertEquals(cpfMotorista.equals(driver.switchTo().activeElement()), false);
         Assert.assertEquals(cpfMotorista.getText(), "");
 
 
-        WebElement estadoMotorista = driver.findElement(By.id("estadoMotorista"));
+        MobileElement estadoMotorista = driver.findElement(By.id("estadoMotorista"));
         Assert.assertEquals(estadoMotorista.getText(), "Acre - AC");
 
 
-        nomeMotorista.click();
-        nomeMotorista.clear();
-
-
-        Assert.assertEquals(nomeMotorista.equals(driver.switchTo().activeElement()), true);
-        Assert.assertEquals(nomeMotorista.getText(), "");
-
-
-        nomeMotorista.click();
-        nomeMotorista.clear();
-
-
-        Assert.assertEquals(nomeMotorista.equals(driver.switchTo().activeElement()), false);
-        Assert.assertEquals(nomeMotorista.getText(), "");
-
-
-        WebElement abrirListaMercadorias = driver.findElement(By.id("abrirListaMercadorias"));
+        MobileElement abrirListaMercadorias = driver.findElement(By.id("abrirListaMercadorias"));
         abrirListaMercadorias.click();
 
 
