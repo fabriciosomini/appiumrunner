b0052f20d27ccd755fb2d41201067ca03bf4cc78
fabri <fabricio.somini@gmail.com>
2018-05-31 13:56:36 -0300

    Adicionado: checarInicializacao

--- unesctcc3/src/main/java/unesc/com/unesctcc3/motor/GeradorTestes.java
+++ unesctcc3/src/main/java/unesc/com/unesctcc3/motor/GeradorTestes.java
@@ -9,13 +9,16 @@ import unesc.com.unesctcc3.utilitarios.UtilitarioMetodos;
 
 public class GeradorTestes {
     private static AlgoritmoRegistro algoritmoRegistro;
+    private static boolean inicializado;
 
     public static void inicializar(AlgoritmoRegistro algoritmoRegistro) {
+        inicializado = true;
         GeradorTestes.algoritmoRegistro = algoritmoRegistro;
         GeradorTestes.algoritmoRegistro.registrarEstadoAparelho();
     }
 
     public static Atividade gerarTesteElemento(View elemento) {
+        checarInicializacao();
         if (elemento == null) {
             throw new RuntimeException("O elemento não pode ser nulo");
         }
@@ -23,7 +26,15 @@ public class GeradorTestes {
         return gerarTesteElemento(id);
     }
 
+    private static void checarInicializacao() {
+        if(!inicializado){
+            throw new RuntimeException("Esta operação não pode ser realizada, " +
+                    "pois não houve uma chamada para o método de inicialização.");
+        }
+    }
+
     public static Atividade gerarTesteElemento(String id) {
+        checarInicializacao();
         if (id == null) {
             throw new RuntimeException("O identificador do elemento não pode ser nulo");
         }
@@ -33,22 +44,27 @@ public class GeradorTestes {
     }
 
     public static void terminarTeste() {
+        checarInicializacao();
         algoritmoRegistro.parar();
     }
 
     public static void terminarTeste(String nomeTeste) {
+        checarInicializacao();
         algoritmoRegistro.parar(nomeTeste);
     }
 
     public static String getTeste() {
+        checarInicializacao();
         return algoritmoRegistro.getScript();
     }
 
     public static EstadoDispositivoUtilitario.EstadoAparelhoMovel getEstadoAparelhoMovel() {
+        checarInicializacao();
         return algoritmoRegistro.getEstadoAparelhoMovel();
     }
 
     public static void pressionar(Atividade.Tecla tecla) {
+        checarInicializacao();
         Atividade atividade = new Atividade(algoritmoRegistro);
         atividade.pressionarTeclas(tecla).reproduzirAcoes();
     }
